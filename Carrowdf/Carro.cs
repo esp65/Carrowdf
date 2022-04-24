f2e3e89daeca26753732223be9101c2ad5820abc
espinoestefani579@gmail.com <espinoestefani579@gmail.com>
2022-04-23 23:00:02 -0700

    Agregar archivos de proyecto.

---
+++ Carrowdf/Carro.cs
@@ -0,0 +1,18 @@
+﻿namespace Carrowdf
+{
+    internal class Carro
+    {
+        private string mar;
+        private int modl;
+        private string maxvel;
+        private string propietario;
+
+        public Carro(string mar, int modl, string maxvel, string propietario)
+        {
+            this.mar = mar;
+            this.modl = modl;
+            this.maxvel = maxvel;
+            this.propietario = propietario;
+        }
+    }
+}
---
+++ Carrowdf/conducir_carro.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Carrowdf
+{
+    /// <summary>
+    /// Lógica de interacción para conducir_carro.xaml
+    /// </summary>
+    public partial class conducir_carro : Window
+    {
+        Carrito1 carrito;
+        
+        public conducir_carro(Carrito1 _tempcarr)
+        {
+            InitializeComponent();
+            carrito = _tempcarr;
+            datos.Content = $"Marca: {carrito.marca} Propietario: {carrito.owner} Modelo: {carrito.modelo} Maxima Velocidad: {carrito.maxvelocidad}";
+            
+        }
+
+        private void acelerar_carro_Click(object sender, RoutedEventArgs e)
+        {        
+            lblvelocidad.Content = carrito.acelerar();
+            
+            
+        }
+
+        private void encendido_Click(object sender, RoutedEventArgs e)
+        {
+            
+            lblestatus.Content = carrito.encender_motor();
+        }
+
+        private void btfrenar_Click(object sender, RoutedEventArgs e)
+        {
+            lblfrenar.Content = carrito.frenar();
+        }
+
+        private void btbocinar_Click(object sender, RoutedEventArgs e)
+        {
+            lblbocina.Content = carrito.bocinar();
+        }
+
+        private void apagado_Click(object sender, RoutedEventArgs e)
+        {
+            lblapa.Content = carrito.apagar_motor();
+        }
+
+        private void desacelerar_Click(object sender, RoutedEventArgs e)
+        {
+            lbldesa.Content = carrito.desacelerar();
+        }
+    }
+}
